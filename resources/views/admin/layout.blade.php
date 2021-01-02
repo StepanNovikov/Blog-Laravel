5632a9cae7262ff81454e54e400f5a422ed84523
Stepan <stepan.novikov.1996.com>
2021-01-02 15:53:02 +0300

    CRUD for categories

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -6,7 +6,7 @@
   <title>AdminLTE 2 | Blank Page</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="css/admin.css">
+  <link rel="stylesheet" href="/Blog-Laravel/public/css/admin.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -437,7 +437,7 @@
   <div class="control-sidebar-bg"></div>
 </div>
 <!-- ./wrapper -->
-<script src="js/admin.js"></script>
+<script src="/Blog-Laravel/public/js/admin.js"></script>
 </body>
 
 <!-- Mirrored from almsaeedstudio.com/themes/AdminLTE/pages/examples/blank.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 18 Dec 2016 15:13:35 GMT -->
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin', 'Admin\DashboardController@index');
+Route::group(['prefix' => 'admin', 'namespace' => 'admin'], function () {
+    Route::get('/', 'DashboardController@index');
+    Route::resource('/categories', 'CategoriesController');     
+});
+
